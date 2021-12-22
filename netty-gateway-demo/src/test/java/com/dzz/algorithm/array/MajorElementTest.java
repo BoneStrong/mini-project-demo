f24aad18ffff741d17b6e81bfa48581e9008f57e
zoufeng <zoufeng@dzz.com>
2021-12-22 18:00:46 +0800

    add lcs

--- netty-gateway-demo/src/test/java/com/dzz/algorithm/array/MajorElementTest.java
+++ netty-gateway-demo/src/test/java/com/dzz/algorithm/array/MajorElementTest.java
@@ -2,6 +2,9 @@ package com.dzz.algorithm.array;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class MajorElementTest {
 
     /*
@@ -45,7 +48,7 @@ public class MajorElementTest {
                 j--;
             } else {
                 int temp = i;
-                while (arr[temp] == arr[j] && temp < j) {
+                while (arr[temp] == arr[j] && temp < j) {//相等则后右边的指针交换，然后一起消消乐
                     temp++;
                 }
                 int t = arr[temp];
@@ -54,5 +57,83 @@ public class MajorElementTest {
             }
         }
         return arr[i];
+    }
+
+
+    /*
+     * 多数元素进阶（水帖王问题进阶）
+     *
+     * 社区有k个水贴王，他们发的贴子都超过了全部贴子的1/(k+1),求出这几个水王.
+     *
+     * 比如有3个水王，他们发的贴子都超过了1/4，求这3个水王
+     *
+     * 分析：
+     * 如果按照消消乐法，则需要证明这么一个问题
+     * 假设某个水王的贴子数是X，贴子总数是N，N>=K+1, X/N>1/K,
+     * 则K+N<N<KX, N-K<KX-K
+     *  ((X-1)/(N-K-1))> ((X-1)/(KX-K))=1/(K+1)
+     *
+     * 所以每次消除K+1个不同的数字，剩下的水王贴子肯定还是大于1/（1+k）的
+     *
+     * 但问题是：最后需要剩下多少贴子合适呢？
+     * 因为如果剩下
+     *
+     *
+     *
+     * */
+    private int[] majorityElementPro(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>(k + 1);
+        int count = 0;
+        int i = 0;
+        while (i <= (arr.length - k)) {
+            if (count == k + 1) {
+                count = 0;
+                map.clear();
+                continue;
+            }
+            if (map.get(arr[i]) == null) {
+                map.put(arr[i], i);
+                count++;
+                i++;
+            } else {
+                int temp = i + 1;
+
+                while (temp < arr.length) {
+                    if (map.get(arr[temp]) == null) {
+                        map.put(arr[temp], temp);
+                        count++;
+
+                        //swap
+                        int t = arr[temp];
+                        arr[temp] = arr[i];
+                        arr[i] = t;
+
+                        i++;
+                        break;
+                    }
+                    temp++;
+                }
+                if (temp == arr.length) break;
+            }
+        }
+
+        map.clear();
+        int[] result = new int[k];
+        int j = 0;
+        while (i < arr.length) {
+            if (map.get(arr[i]) == null) {
+                map.put(arr[i], i);
+                result[j] = arr[i];
+                j++;
+            }
+            i++;
+        }
+        return result;
+    }
+
+
+    @Test
+    public void test2() {
+        System.out.println(Arrays.toString(majorityElementPro(new int[]{2, 2, 4, 5, 1, 3, 1, 1, 3, 3, 2}, 3)));
     }
 }
