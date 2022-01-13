8535ccfb2afe50eb9f2392ab5ba7c4d97fe9c740
zoufeng <zoufeng@dzz.com>
2022-01-13 09:33:24 +0800

    add ignite

---
+++ netty-gateway-demo/src/test/java/com/dzz/algorithm/linklist/LinkNumAddOrMultiply.java
@@ -0,0 +1,46 @@
+package com.dzz.algorithm.linklist;
+
+import org.junit.Test;
+
+public class LinkNumAddOrMultiply {
+
+    /*
+     * 单链表大数相加
+     * 两个数为正数。
+     *
+     * 用链表求和：
+     * 样例一
+     *
+     * 2 3 4 5
+     * 4 5 6
+     * 2 8 0 1
+     *
+     * 样例二
+     *
+     * 9 9 9
+     * 9 9 9
+     * 1 9 9 9
+     *
+     * 单链表大数相乘
+     *
+     *
+     * */
+    @Test
+    public void test(){
+
+    }
+
+    /*
+    * plus
+    *
+    *
+    * */
+    public ListNode add(ListNode node1,ListNode node2){
+        return null;
+    }
+
+
+    public ListNode multiply(ListNode node1,ListNode node2){
+        return null;
+    }
+}
